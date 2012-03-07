1241fde485bde6f44bd6dcf642c0d2b8dbbda940
Mike Edwards <mikeedwards83@googlemail.com>
2012-03-07 08:15:14 +0000

    Tutorial 1

---
+++ Tutorial1/Source/Glass.Sitecore.Mapper.Tutorial/layouts/GlassTutorial1.ascx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Glass.Sitecore.Mapper.Tutorial.Models;
+
+namespace Glass.Sitecore.Mapper.Tutorial.layouts
+{
+    public partial class GlassTutorial1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ISitecoreContext context = new SitecoreContext();
+            var home = context.GetCurrentItem<Home>();
+
+            Title.Text = home.Title;
+            Text.Text = home.Text;
+            CreateBy.Text = home.CreatedBy;
+            CreatedOn.Text = home.Created.ToShortDateString();
+            Path.Text = home.Path;
+        }
+    }
+}
